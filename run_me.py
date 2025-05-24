f64dc44ff8533d81a56d6dee8c87bd7f7a73d1f9
qbasd-eu <randomstuff@qbasd.eu>
2025-05-24 18:21:28 +0200

    If run_me.py is run and any of the files the rest of the code relies on exist, they get deleted, to make sure there are no issues

--- run_me.py
+++ run_me.py
@@ -1,3 +1,15 @@
+import os
+import shutil
+
+if os.path.exists('process_song/beats_array.json'):
+    os.remove('process_song/beats_array.json')
+
+if os.path.exists('beatmap.zip'):
+    os.remove('beatmap.zip')
+
+if os.path.exists('beatmap'):
+    shutil.rmtree('beatmap')
+
 import marker
 import convert_to_dat
 import zipping
